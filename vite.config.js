e504a60fa4a6c16be7f0320bfa1f8ec9d25f4161
Igniske <104842998+Igniske@users.noreply.github.com>
2023-07-28 11:20:56 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "https://igniske.github.io//landingpage/"
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/landingpage/'
+  }
+
+  return config
 })
